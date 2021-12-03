e81d303bd990748c4bd80c20a8c630a1e97eacf8
Eran Toledano <erant10@gmail.com>
2021-12-03 13:17:36 -0800

    Update dispose.ps1

--- .github/workflows/dispose.ps1
+++ .github/workflows/dispose.ps1
@@ -1,1 +1,6 @@
-Write-Output "Delete Source Control"
+Write-Output "Deleting Source Control"
+
+$sourceControlId = $Env:SOURCE_CONTROL_ID
+$Creds = $env.CREDS | ConvertFrom-Json
+Write-Host $Creds.tenantId
+Write-Host $Creds.subscriptionId
