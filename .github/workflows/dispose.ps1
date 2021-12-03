211f0513dc3b05fc4294e51b576d15771c46bfe7
Eran Toledano <erant10@gmail.com>
2021-12-03 13:32:26 -0800

    Update dispose.ps1

--- .github/workflows/dispose.ps1
+++ .github/workflows/dispose.ps1
@@ -1,6 +1,14 @@
 Write-Output "Deleting Source Control"
 
 $sourceControlId = $Env:SOURCE_CONTROL_ID
-$Creds = $env.CREDS | ConvertFrom-Json
-Write-Host $Creds.tenantId
-Write-Host $Creds.subscriptionId
+$Creds = $Env:CREDS | ConvertFrom-Json
+$endpoint = "management.azure.com"
+$subscriptionId = $Creds.subscriptionId
+$tenantId = $Creds.tenantId
+$resourceGroupName = "test-rg" # $Env:RESOURCE_GROUP_NAME
+$workspaceName = "test-rws" # $Env:WORKSPACE_NAME
+Write-Host $subscriptionId
+Write-Host $tenantId
+
+$url = "https://$endpoint/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.OperationalInsights/workspaces/$workspaceName/providers/Microsoft.SecurityInsights/sourceControls/$sourceControlId?api-version=2021-03-01-preview"
+Write-Host $url
